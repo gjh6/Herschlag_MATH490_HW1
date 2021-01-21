b4d83d8711270f8489a530a92c6482f289c83150
Gregory Herschlag <gjh@math.duke.edu>
2021-01-21 14:17:52 -0500

    updating w/ files

---
+++ lib/sphereSampling.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def inUnitSphere(x):
+    # Your code
+    return # value
+
+
+def sampleInsideUnitSphere(dim, rng, nSamples, bnds):
+    # Your code
+    return # value
+
+
+def samplePointsFromUnitSphere(dim, nSamples):
+    # Your code
+    return # value
